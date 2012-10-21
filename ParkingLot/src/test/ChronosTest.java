28b663fbe94bc4aaf418b276f5e3f98952ba6ff8
Vaibhav Verma <vaibhav2614@gmail.com>
2012-10-21 01:25:48 -0400

    more goodness in ChronosTest

--- ParkingLot/src/test/ChronosTest.java
+++ ParkingLot/src/test/ChronosTest.java
@@ -15,11 +15,9 @@ public class ChronosTest extends SetupTest
 
         Thread.sleep(1000);
         
-        System.out.println("trafficGenerator.size ="+trafficGenerator.subscribers.size());
-        System.out.println("gates.size ="+gates.size());
         assert(trafficGenerator.subscribers.size() == gates.size());
 
-        System.out.println("We subscribed all the gates brah");
+        System.out.println("We time subscribed all the gates brah");
     }
 
     public static void main(String[] args) throws Exception
