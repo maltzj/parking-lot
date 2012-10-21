e402b3bab4b6d1481dbdec96196eac2a526323dc
William Brozas <wbrozas@gmail.com>
2012-10-20 21:56:45 -0400

    Fall 2000 Secure-E-commerce

---
+++ ParkingLot/src/test/Config.java
@@ -0,0 +1,45 @@
+import java.net.InetAddress;
+
+public class Config
+{
+	public HostPort trafficGenerator;
+	public HostPort [] gates;
+	public Config()
+	{
+		try
+		{
+			trafficGenerator = new HostPort();
+			trafficGenerator.iaddr = InetAddress.getByName("localhost");
+			trafficGenerator.port = 6000;
+			gates = new HostPort [6];
+			for(int i = 0; i < 6; i++)
+			{
+				gates[i] = new HostPort();
+				gates[i].iaddr = InetAddress.getByName("localhost");
+				gates[i].port = 6001 + i;
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void printConfig()
+	{
+		System.out.println(trafficGenerator.iaddr + " " + trafficGenerator.port);
+		for(int i = 0; i < 6; i++)
+		{
+			System.out.println(gates[i].iaddr + " " + gates[i].port);
+		}	
+	}
+	public class HostPort
+	{
+		public InetAddress iaddr;
+		public int port;
+	}
+	/*public static void main(String [] args)
+	{
+		Config c = new Config();
+		c.printConfig();
+	}*/
+}
