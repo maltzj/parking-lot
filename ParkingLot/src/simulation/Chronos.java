575875c2b10423e2de8b75e2a1c5ec7884a902e4
Jerry Reptak <jreptak@gmail.com>
2012-10-20 23:27:34 -0400

    Chronos

--- ParkingLot/src/simulation/Chronos.java
+++ ParkingLot/src/simulation/Chronos.java
@@ -1,11 +1,9 @@
 package simulation;
 
 import messaging.TimeSubscribeMessage;
-import java.util.ArrayList;
 
 public interface Chronos 
 {	
-	private ArrayList<HostPort> subscribers;
 	public long getCurrentTime();
 	public void onSubscribeReceived(TimeSubscribeMessage messageReceived);
 	public void publish();
