39aef5b612d7b7e61572dcd52fa1dc837dbc8a11
Jonathan Maltz <jm1271@gmail.com>
2012-10-23 04:02:32 -0400

    Docs on the SimulationMessageListener

--- ParkingLot/src/gates/SimulationMessageListener.java
+++ ParkingLot/src/gates/SimulationMessageListener.java
@@ -5,11 +5,21 @@ import java.net.Socket;
 
 import messaging.AbstractMessage;
 
+/**
+ * This is an abstraction which allows the gates to communicate with the simulation.  
+ * It is just a runnable that sits and continually decodes messages and passes them onto the gate for action
+ *
+ */
 public class SimulationMessageListener implements Runnable {
 
 	private GateImpl gateListeningFor;
 	Socket socketListeningOn;
 	
+	/**
+	 * Creates a simulation listener which listens on the given socket
+	 * @param gateListening The gate which will act upon the received messages
+	 * @param listeningFor The socket upon which this is listening.
+	 */
 	public SimulationMessageListener(GateImpl gateListening, Socket listeningFor)
 	{
 		this.socketListeningOn = listeningFor;
@@ -18,7 +28,7 @@ public class SimulationMessageListener implements Runnable {
 	
 	@Override
 	public void run() {
-		while(!gateListeningFor.die)
+		while(!gateListeningFor.die) //Keep running until we give it permission to die
 		{
 			AbstractMessage messageReceived;
 			try {
@@ -35,6 +45,11 @@ public class SimulationMessageListener implements Runnable {
 		}
 	}
 	
+	/**
+	 * Writes a message to the socket that this object is listening on
+	 * @param messageToSend The message which is being sent.
+	 * @throws IOException
+	 */
 	public void writeMessage(AbstractMessage messageToSend) throws IOException
 	{
 		AbstractMessage.encodeMessage(socketListeningOn.getOutputStream(), messageToSend);
