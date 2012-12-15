4f320fbb12ef8a6f0c0d04e586ddd688e1536780
Vaibhav Verma <vaibhav2614@gmail.com>
2012-12-15 14:18:52 -0500

    Removing files we don't need

--- ParkingLot/src/test/Bootstrap.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Bootstrap
-{
-    public static void main(String[] args) throws Exception
-    {
-        //TrafficGenerator derp = new TrafficGenerator(Config.getSharedInstance().managers);
-    }
-}
